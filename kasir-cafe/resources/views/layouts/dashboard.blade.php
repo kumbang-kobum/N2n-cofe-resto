63ad67c005af6cd697f04d0408e412efde33484a
Kumbang-kobum <chandra.irawan17@gmail.com>
2026-02-17 17:25:03 +0700

    logo

--- kasir-cafe/resources/views/layouts/dashboard.blade.php
+++ kasir-cafe/resources/views/layouts/dashboard.blade.php
@@ -4,6 +4,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name','Kasir Cafe') }}</title>
+  <link rel="icon" type="image/png" href="{{ asset('n2Nlogo.png') }}">
+  <link rel="apple-touch-icon" href="{{ asset('n2Nlogo.png') }}">
   @vite(['resources/css/app.css','resources/js/app.js'])
   @stack('styles')
 </head>
